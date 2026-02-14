06352f02d79edca56be5e7b6553d1502171db5ea
wouter-codes <wouter.klinkenberg@outlook.com>
2026-02-14 09:15:53 +0000

    Add static files and templates for Taskmaster app, update views to render tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,17 @@
-from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+from django.http import HttpRequest
+from django.http.response import HttpResponse
+from .models import Task, Category
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Taskmaster: tasks app is running.")
+    tasks_todo = Task.objects.filter(completed=False).order_by("-created_at")
+    tasks_done = Task.objects.filter(completed=True).order_by("-created_at")
+    
+    return render(
+        request, "index.html", 
+        {"tasks_todo": tasks_todo,
+         "tasks_done": tasks_done,
+        }
+    )
+
